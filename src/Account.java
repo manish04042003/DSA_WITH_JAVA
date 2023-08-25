ff647ac0e6459240e01db984f8fe806b11628ac4
manish04042003 <manishkumar817835@gmail.com>
2023-08-25 14:15:37 +0530

    remove extra File

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+
+
+public  class Account {
+    public static void main(String[] args) {
+        Balance b = new Balance();
+//        int ans = b.getX();
+//        System.out.println(ans);
+//        b.setX(20);
+//        int ans1 = b.getX();
+//        System.out.println(b.x);
+
+        System.out.println('manish');
+    }
+}
+
+class Balance{
+    private int x = 100 ;
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+}
